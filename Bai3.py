be1c8349de1d8a63b59efac35a96b6d1e6b4f5d0
nguyenxuanson01012007-ship-it <nguyenxuanson01012007@gmail.com>
2026-05-20 14:00:47 +0700

    Update Bai3.py

--- Bai3.py
+++ Bai3.py
@@ -1,23 +1,19 @@
-print('Khởi tạo hồ sơ nhân sự');
-employees = [];
-for employee in range(1,4):
-    print('Thông tin nhân viên ', employee);
-    id_emp = input('Nhập mã nhân viên: ');
-    name_emp = input('Nhập tên nhân viên: ');
-    dept_emp = input('Nhập phòng ban nhân viên: ');
-    employees.append({
-        'id' : id_emp,
-        'name' : name_emp,
-        'dept' : dept_emp
-    })
+    for i in range(1,4):
+    print("Thông tin của nhân viết thứ: ", i)
+    number_id = int(input("Mã nhân viên: "))
+    full_name = input("Họ và tên nhân viên: ")
+    department = input("Phòng ban công tác: ")
 
-if id_emp =="" or name_emp == "" : 
-    print ("[CANH BAO] Dữ Liệu tên hoặc mã không hợp lệ! Hủy bỏ tạo hồ sơ cho nhân viên này.")
+    if number_id == " " or (full_name == " "):
+        print("[CẢNH BÁO] Dữ liệu tên hoặc mã không hợp lệ! Hủy bỏ tạo hồ sơ cho nhân viên này.")
+        continue
 
-else :
+    print("\n--- Phiếu Hồ sơ Điện tử ----")
+    print("Mã nhân viên: ", number_id)
+    print("Họ và tên nhân viên: ", full_name)
+    print("Phòng ban công tác: ", number_id)
+    print("----------------------------\n")
 
-    print('Hồ sơ sau khi tiếp nhận');
-    for emp in employees:
-        print('Mã nhân viên là: ', emp['id']);
-        print('Tên nhân viên là: ', emp['name']);
-        print('Phòng ban nhân viên là: ', emp['dept']);
+print("Đã hoàn tất quá trình duyệt thường cho 3 nhân viên!")
+
+    
